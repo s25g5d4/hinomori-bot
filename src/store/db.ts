f63de7318ef6660bba54d2dbacab38449f42cfb6
s25g5d4 <s25g5d4@gmail.com>
2022-02-05 21:32:02 +0800

    Add log messages to db and store files

--- src/store/db.ts
+++ src/store/db.ts
@@ -1,14 +1,17 @@
 import { initializeApp, applicationDefault } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
+import { logger } from "../logger";
 
 export class FirebaseDB {
   db: FirebaseFirestore.Firestore;
 
   async init(): Promise<void> {
+    logger.debug("initializing Firebase");
     initializeApp({
       credential: applicationDefault(),
     });
 
     this.db = getFirestore();
+    logger.info("Firebase initialized");
   }
 }
--- src/store/user-profiles.ts
+++ src/store/user-profiles.ts
@@ -1,5 +1,6 @@
 import { FirebaseDB } from "./db";
 import { UserProfileRecord } from "../models/user-profile";
+import { logger } from "../logger";
 
 export class UserProfileStore {
   private readonly collectionName = "user-profiles";
@@ -8,24 +9,25 @@ export class UserProfileStore {
   constructor(private db: FirebaseDB) {}
 
   async init(): Promise<void> {
+    logger.debug("initializing UserProfileStore");
     this.collection = this.db.db.collection(this.collectionName);
-    return;
+    logger.info("UserProfileStore initialized");
   }
 
   async get(user: string): Promise<UserProfileRecord | null> {
+    logger.debug({ user }, "get user-profiles record");
     const doc = await this.collection.doc(user).get();
     if (!doc.exists) {
+      logger.info({ user }, "user-profiles record not found");
       return null;
     }
+    logger.info({ user }, "user-profiles record retrieved");
     return doc.data() as UserProfileRecord;
   }
 
-  async has(user: string): Promise<boolean> {
-    const doc = await this.collection.doc(user).get();
-    return doc.exists;
-  }
-
   async set(user: string, record: UserProfileRecord): Promise<void> {
+    logger.debug({ user }, "update user-profiles record");
     await this.collection.doc(user).set(record);
+    logger.info({ user }, "user-profiles record updated");
   }
 }
