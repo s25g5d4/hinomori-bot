38198ee7931f21437304f10268e67c5cc815dd3e
s25g5d4 <s25g5d4@gmail.com>
2022-03-23 11:54:56 +0800

    Add test case for pole position

--- test/models/pole-position.ts
+++ test/models/pole-position.ts
@@ -6,7 +6,7 @@ import {
 } from "../../src/models/pole-position";
 
 describe("Pole Position", function () {
-  it("should return correct pole position", function () {
+  it("should return correct pole position (5 players)", function () {
     const testData1: PlayerRatio[] = [
       { ratio: 1 },
       { ratio: 2 },
@@ -24,6 +24,24 @@ describe("Pole Position", function () {
       { ratio: 1 },
     ];
     expect(polePosition(testData2)).to.deep.equal([0, 2, 4, 3, 1]);
+  });
+
+  it("should return correct pole position (4 players)", function () {
+    const testData1: PlayerRatio[] = [
+      { ratio: 1 },
+      { ratio: 2 },
+      { ratio: 3 },
+      { ratio: 4 },
+    ];
+    expect(polePosition(testData1)).to.deep.equal([2, 1, 0, 3]);
+
+    const testData2: PlayerRatio[] = [
+      { ratio: 3 },
+      { ratio: 5 },
+      { ratio: 4 },
+      { ratio: 1 },
+    ];
+    expect(polePosition(testData2)).to.deep.equal([2, 0, 3, 1]);
   });
 
   it("should throw error if less than 4 players are given", function () {
